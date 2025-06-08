9266ca9575bfd205641716c8e58fa0b9cba8cbb5
Lena778899 <werewolf8@yandex.ru>
2025-06-08 19:03:01 +0300

    Add files via upload

---
+++ queries.sql
@@ -0,0 +1,4 @@
+SELECT
+COUNT(customer_id) AS customers_count 
+FROM customers;
+-- Вывести одну колонку. Использую функцию COUNT(), чтобы подсчитать строки в колонке customer_id, использую AS чтобы присвоить колонке с результатом имя customers_count, из таблицы customers.
